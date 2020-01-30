176e0eacc63e30dfa45ec34d92211a4d6c346211
VIVEK KUMAR <vivekofibm@gmail.com>
2020-01-30 20:03:41 +0530

    Add files via upload
    Creation of AWS instance through Terraform (Basic)

---
+++ Create_first_intance_in_aws.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  access_key = "AKIA57EZYXTV5"     								#\\Here u have to give ur "access_key" which u have to find it in aws
+  secret_key = "qtHpzKqWLHAYMZDMx6F"					     	#\\Here u have to give ur "secret_key" which u have to find it in aws
+  region     = "ap-south-1"     								#\\mention the region where u want to launch your instance
+ 
+}
+
+===================================================================================================
+#creating aws instance
+===================================================================================================
+
+resource "aws_instance" "terraform1" {
+  ami           = "ami-01e074f40dfb9999d"						#\\ Give the ami id(image id [or] os id )(this image id is free tier)
+  instance_type = "t2.micro"									#\\ Give the type of instace u want(this one is free tier)
+  security_groups = "default"									#\\ Give the Security group name [or] u can give it as default
+  key_name = "vivekkumar"										#\\ Give the key_name	
+
+
+}
