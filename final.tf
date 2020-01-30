deb00678e5068f9bc0f53330668fcc2587917712
VIVEK KUMAR <vivekofibm@gmail.com>
2020-01-30 20:17:43 +0530

    Update final.tf

--- final.tf
+++ final.tf
@@ -1,28 +1,36 @@
 provider "aws" {
-  access_key = "AKIA57VVTMAXU"
-  secret_key = "qtHpzKqWLHAYMZDMx6F9h9zypwI0"
-  region     = "ap-south-1"
+  access_key = "AKITMAXUEZYXTV5"     						#\\Here u have to give ur "access_key" which u have to find it in aws
+  secret_key = "qtH9zypwI0YaBjsPAZp0oZ"     	#\\Here u have to give ur "secret_key" which u have to find it in aws
+  region     = "ap-south-1"     								#\\mention the region where u want to launch your instance
+ 
 }
+=================================================================================================
+#creating Security group in aws 
+=================================================================================================
 
-resource "aws_security_group""efs-general"{
-        name ="Second_security_grp"
+resource "aws_security_group""group1"{
+        name ="fiv_security_grp"
         description="Allow all traffic"
-		
-		ingress	{
-			from_port=All
-			to_port=All
-			protocol="All"
-		}
-		
+        vpc_id = "vpc-31555a59"
+
+        ingress {
+                    from_port=0000
+                    to_port=000
+                    protocol="-1"        						#\\Here "-1"  is for all traffic if u need specific one then give the specific name
+                    cidr_blocks=["0.0.0.0/0"]
+
+                }
+
+===================================================================================================
+#creating aws instance
+===================================================================================================
+
+resource "aws_instance" "terraform1" {
+  ami           = "ami-01e074f40dfb9999d"						#\\ Give the ami id(image id [or] os id )(this image id is free tier)
+  instance_type = "t2.micro"									#\\ Give the type of instace u want(this one is free tier)
+  security_groups = "Second_security_grp"						#\\ Give the Security group name [or] u can give it as default
+  key_name = "vivekkumar"										#\\ Give the key_name	
 
 
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-01e074f40dfb9999d"
-  instance_type = "t2.micro"
-  security_groups = "Second_security_grp"
-  key_name = "vivekkumar"
-
-
-}
